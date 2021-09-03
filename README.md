5b7577167c0e0e6c82f05874da84e49eafda8bee
attapun-an <attapunanivat@gmail.com>
2021-09-03 16:38:55 +0100

    pun intergrated hemiphot code and added example images

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 Coding Repository For EES Group 5
 
 
-Useful links
-- https://imagej.net/scripting/macro
+#### Useful links
+- [Documentation for imageJ macro](https://imagej.net/scripting/macro)
+- [Hemi photo guide GitHub Repo](https://github.com/johngodlee/hemi_photo_guide) (contains all the code)
---
+++ scripts/test-script.R
@@ -0,0 +1,13 @@
+# test script to practice using Hemiphot functions 
+# attapun-an | attapunanivat@gmail.com
+# uses images from wikipedia under images/test
+
+# Working directory is set to root directory (one above the script folder)
+rm(list=ls())  # clear memory
+
+
+# load libraries and source files ----
+library(jpeg)
+source("scripts/Hemiphot/Hemiphot.R")
+
+im <- 
